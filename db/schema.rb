da5153be1ee648b50e876024627eef5c73bbc30b
Sam Lim <limsammy1@gmail.com>
2017-08-30 14:01:47 -0600

    Created Category model and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214181533) do
+ActiveRecord::Schema.define(version: 20170830200121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.bigint "jobs_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jobs_id"], name: "index_categories_on_jobs_id"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -32,5 +40,6 @@ ActiveRecord::Schema.define(version: 20161214181533) do
     t.index ["company_id"], name: "index_jobs_on_company_id"
   end
 
+  add_foreign_key "categories", "jobs", column: "jobs_id"
   add_foreign_key "jobs", "companies"
 end
