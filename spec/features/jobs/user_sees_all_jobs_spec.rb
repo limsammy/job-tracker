2c12055440c084d52cb7dc5badc2b82a67506875
Sam Lim <limsammy1@gmail.com>
2017-09-01 15:27:58 -0600

    Got User Sees All Jobs spec to pass

--- spec/features/jobs/user_sees_all_jobs_spec.rb
+++ spec/features/jobs/user_sees_all_jobs_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "User sees all jobs" do
   scenario "a user sees all the jobs for a specific company" do
-    company = Company.create!(name: "ESPN")
-    company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-    company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City")
+    company = create(:company, name: "ESPN")
+    create(:job, title: "Developer", company_id: company.id)
+    create(:job, title: "QA Analyst", company_id: company.id)
 
     visit company_path(company)
 
