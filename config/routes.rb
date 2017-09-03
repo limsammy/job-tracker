f04341378233b4e0849c9b0a865c2fa1ba00c60d
Sam Lim <limsammy1@gmail.com>
2017-09-03 03:39:17 -0700

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-
   resources :companies do
     resources :jobs
   end
 
   resources :jobs, only: [:index, :edit, :show, :update, :destroy]
+
+  resources :jobs, only: [:show] do
+    resources :comments, only: [:create]
+  end
 
   resources :categories
 end
