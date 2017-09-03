3cd7cfe1661d4c7e807deee26b0529d8465534c6
Sam Lim <limsammy1@gmail.com>
2017-09-03 03:38:27 -0700

    Set up controller, now for routes

--- spec/features/comments/user_creates_a_new_comment_spec.rb
+++ spec/features/comments/user_creates_a_new_comment_spec.rb
@@ -3,7 +3,7 @@ describe "Creating a comment" do
     company = create(:company)
     job = create(:job, company_id: company.id)
     visit company_job_path(job.company, job)
-    fill_in "comment[content]", with: "This Is Comment Text"
+    fill_in "comment[body]", with: "This Is Comment Text"
     click_on "Create Comment"
     expect(page).to have_current_path(company_job_path(job.company, job))
     expect(page).to have_content("This Is Comment Text")
--- spec/features/comments/user_sees_properly_formatted_comments_spec.rb
+++ spec/features/comments/user_sees_properly_formatted_comments_spec.rb
@@ -3,9 +3,9 @@ describe "Order of multiple comments" do
     company = create(:company)
     job = create(:job, company_id: company.id)
     visit company_job_path(job.company, job)
-    fill_in "comment[content]", with: "First Comment Text"
+    fill_in "comment[body]", with: "First Comment Text"
     click_on "Create Comment"
-    fill_in "comment[content]", with: "More Recent Comment Text"
+    fill_in "comment[body]", with: "More Recent Comment Text"
     click_on "Create Comment"
     expect("More Recent Comment Text").to appear_in_page_before("First Comment Text")
   end
